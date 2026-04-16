21eaa50f54bc5e0fec03b7ed5c50b8ab4866d9e7
Mahabub Alahi <mahabubalahi9531@gmail.com>
2026-04-16 14:54:15 +0600

    Refactor Pinata services and update environment variables
    - Replaced `PINATA_API_KEY` and `PINATA_API_SECRET` with `PINATA_JWT` and `PINATA_GATEWAY` in the `.env.example` file.
    - Updated `pinFileToIPFS` to accept a file path instead of a stream.
    - Refactored the Pinata service methods to use the new Pinata SDK.
    - Removed unused dependencies and cleaned up the code for better readability.

--- services/pinataServices.js
+++ services/pinataServices.js
@@ -1,173 +1,134 @@
 require('dotenv').config();
-const { PINATA_API_KEY, PINATA_API_SECRET, DESIRED_GATEWAY_PREFIX } = process.env;
-const pinataSDK = require('@pinata/sdk');
-const pinata = new pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
-const IPFSGatewayTools = require("@pinata/ipfs-gateway-tools/dist/node");
-const gatewayTools = new IPFSGatewayTools();
+const { PinataSDK } = require('pinata');
+const fs = require('fs');
+const path = require('path');
+
+const { PINATA_JWT, PINATA_GATEWAY, DESIRED_GATEWAY_PREFIX } = process.env;
+
+const pinata = new PinataSDK({
+    pinataJwt: PINATA_JWT,
+    pinataGateway: PINATA_GATEWAY,
+});
 
 //Pin File to IPFS
-const pinFileToIPFS = async (readableStreamForFile, options) => {
+const pinFileToIPFS = async (filePath, options = {}) => {
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
+        const buffer = fs.readFileSync(filePath);
+        const fileName = options.pinataMetadata?.name || path.basename(filePath);
+        const file = new File([buffer], fileName);
 
-        const result = await pinata.pinFileToIPFS(readableStreamForFile, options);
+        let upload = pinata.upload.public.file(file).name(fileName);
+
+        if (options.pinataMetadata?.keyvalues) {
+            upload = upload.keyvalues(options.pinataMetadata.keyvalues);
+        }
+
+        const result = await upload;
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = result;
         response.msg.push("Successfully pin file to IPFS!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
 }
 
-//Unpin to IPFS
-const unpin = async (hashToUnpin) => {
+//Unpin from IPFS
+const unpin = async (fileId) => {
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
-
-        const result = await pinata.unpin(hashToUnpin);
+        const result = await pinata.files.public.delete([fileId]);
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = result;
-        response.msg.push("Successfully unpin to IPFS!");
+        response.msg.push("Successfully unpinned from IPFS!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
 }
 
 //Pin JSON to IPFS
-const pinJSONToIPFS = async (body, options) => {
+const pinJSONToIPFS = async (body, options = {}) => {
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
+        let upload = pinata.upload.public.json(body);
 
-        const result = await pinata.pinJSONToIPFS(body, options);
+        if (options.pinataMetadata?.name) {
+            upload = upload.name(options.pinataMetadata.name);
+        }
+        if (options.pinataMetadata?.keyvalues) {
+            upload = upload.keyvalues(options.pinataMetadata.keyvalues);
+        }
+
+        const result = await upload;
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = result;
         response.msg.push("Successfully pin JSON to IPFS!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
 }
 
-//Pin by hash to IPFS
-const pinByHash = async (hashToPin) => {
+//Pin by CID to IPFS
+const pinByCID = async (cid, options = {}) => {
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
+        let upload = pinata.upload.public.cid(cid);
 
-        const result = await pinata.pinByHash(hashToPin);
+        if (options.name) {
+            upload = upload.name(options.name);
+        }
+
+        const result = await upload;
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = result;
-        response.msg.push("Successfully pin by hash to IPFS!");
+        response.msg.push("Successfully pin by CID to IPFS!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
 }
 
-//Hash metadata to IPFS
-const hashMetadata = async (ipfsPinHash, metadata) => {
+//Update file metadata
+const updateMetadata = async (fileId, metadata) => {
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
-
-        const result = await pinata.hashMetadata(ipfsPinHash, metadata);
+        const result = await pinata.files.public.update({
+            id: fileId,
+            name: metadata.name,
+            keyvalues: metadata.keyvalues,
+        });
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = result;
-        response.msg.push("Successfully hash metadata to IPFS!");
+        response.msg.push("Successfully updated metadata!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
-    }
-
-    return response;
-}
-
-//Pin from file system to IPFS
-const pinFromFS = async (sourcePath, options) => {
-    let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
-
-    try{
-
-        const result = await pinata.pinFromFS(sourcePath, options);
-        response.error = false;
-        response.status =  200;
-        response.data = result;
-        response.msg.push("Successfully pin file from file system!");
-
-    }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
@@ -180,54 +141,15 @@ const testAuthentication = async () => {
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
-
         const result = await pinata.testAuthentication();
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = result;
         response.msg.push("Successfully test the authentication!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
-    }
-
-    return response;
-}
-
-/**
- * @returns {object} - response object with user pinned total data
- */
-const userPinnedDataTotal = async () => {
-    let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
-
-    try{
-
-        const result = await pinata.userPinnedDataTotal();
-        response.error = false;
-        response.status =  200;
-        response.data = result;
-        response.msg.push("Successfully user's total pinned data!");
-
-    }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
@@ -236,72 +158,50 @@ const userPinnedDataTotal = async () => {
 /** Get pin files list
  * @param {object} filters - filters object
  * @returns {object} - response object with pin list
- * {
-    hashContains?: string | undefined;
-    pinStart?: string | undefined;
-    pinEnd?: string | undefined;
-    unpinStart?: string | undefined;
-    unpinEnd?: string | undefined;
-    pinSizeMin?: number | undefined;
-    pinSizeMax?: number | undefined;
-    status?: string | undefined;
-    pageLimit?: number | undefined;
-    pageOffset?: number | undefined;
-    metadata?: PinataMetadataFilter | undefined;
-   }
  */
-const pinList = async (filters) => {
+const pinList = async (filters = {}) => {
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
-        const result = await pinata.pinList(filters);
+        let query = pinata.files.public.list();
+
+        if (filters.name) query = query.name(filters.name);
+        if (filters.cid) query = query.cid(filters.cid);
+        if (filters.group) query = query.group(filters.group);
+        if (filters.mimeType) query = query.mimeType(filters.mimeType);
+        if (filters.order) query = query.order(filters.order);
+        if (filters.pageLimit) query = query.limit(filters.pageLimit);
+        if (filters.pageToken) query = query.pageToken(filters.pageToken);
+        if (filters.keyvalues) query = query.keyvalues(filters.keyvalues);
+
+        const result = await query;
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = result;
         response.msg.push("Successfully get pin list!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
 }
 
 const convertGatewayUrl = async (sourceUrl) => {
-
     let response = {error: true, status: 400, msg: [], data: {}, errorMsg: []};
 
     try{
         const desiredGatewayPrefix = DESIRED_GATEWAY_PREFIX;
-        const convertedGatewayUrl = gatewayTools.convertToDesiredGateway(
-            sourceUrl,
-            desiredGatewayPrefix
-        );
+        const convertedGatewayUrl = await pinata.gateways.convert(sourceUrl, desiredGatewayPrefix);
         response.error = false;
-        response.status =  200;
+        response.status = 200;
         response.data = convertedGatewayUrl;
         response.msg.push("Successfully convert gateway url!");
 
     }catch (e) {
-        if (e.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            response.msg.push(e.response.data.message);
-            response.errorMsg.push(e.response.data.message);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            response.msg.push(e.message);
-            response.errorMsg.push(e.message);
-        }
+        response.msg.push(e.message);
+        response.errorMsg.push(e.message);
     }
 
     return response;
@@ -309,11 +209,9 @@ const convertGatewayUrl = async (sourceUrl) => {
 
 module.exports = {
     pinList,
-    userPinnedDataTotal,
     testAuthentication,
-    pinFromFS,
-    hashMetadata,
-    pinByHash,
+    updateMetadata,
+    pinByCID,
     pinJSONToIPFS,
     unpin,
     pinFileToIPFS,
